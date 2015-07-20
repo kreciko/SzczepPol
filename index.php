4e19ccb6778326443711837af382e1425ab2eb56
Kordian <kordeq@gmail.com>
2015-07-20 23:40:29 +0200

    initial skeleton

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
-        for( $i = 0 ; $i < 5; $i++ ){
-    	echo "Hello Aptana Studio \n";
-    }
+session_start();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Wstryskarki Polska</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+    <div id="container">
+        <header></header>
+        <div id="body">
+            <div id="menu"></div>
+            <div id="content"> 
+                <?php
+                    for( $i = 0 ; $i < 30; $i++ ){
+                    echo "Hello Aptana Studio ^^ "; ?> 
+                <br />
+                <?php
+                }
+                ?>
+            </div>   
+        </div>
+        <footer>
+            Copyright by SzczepPol
+        </footer>
+    </div>
+</body>
+</html>
